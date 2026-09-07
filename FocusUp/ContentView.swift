ba33ba15451adb04426da50d641e126eb08904e3
Soroush (Alex) <42461721+soroushMahf@users.noreply.github.com>
2026-09-07 13:18:45 +1000

    Initial Commit

---
+++ FocusUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusUp
+//
+//  Created by Soroush Mahfoozi on 7/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusUp/FocusUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusUpApp.swift
+//  FocusUp
+//
+//  Created by Soroush Mahfoozi on 7/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
